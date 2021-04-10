99f6094f9e9f414a433706e6028fefa0c8d9336c
xiaotongli <xiaotongli>
2021-04-10 17:48:53 -0400

    tweak to test

--- __tests__/websockets.js
+++ __tests__/websockets.js
@@ -1,42 +1,42 @@
-const {io} = require('socket.io-client')
+const { io } = require('socket.io-client');
 const expect = require('expect');
 
 describe('Websocket tests', () => {
-  var socket;
+  let socket;
 
-  beforeAll(function(done) {
-    process.env.NODE_ENV = 'test'
-    
+  beforeAll((done) => {
+    process.env.NODE_ENV = 'test';
+
     done();
-  })
-
-  beforeEach(function(done) {
-      // Setup
-      socket = io('http://localhost:3861', {
-          transports: ['websocket']
-      });
-
-      socket.on('connect', function() {
-        console.log('worked...');
-        done();
-      });
-
-      socket.on('disconnect', function() {
-        console.log('disconnected...');
-      })
   });
 
-  afterEach(function(done) {
-        // Cleanup
-        if(socket.connected) {
-            console.log('disconnecting...');
-            socket.disconnect();
-        } else {
-            // There will not be a connection unless you have done() in beforeEach, socket.on('connect'...)
-            console.log('no connection to break...');
-        }
-        done();
+  beforeEach((done) => {
+    // Setup
+    socket = io('http://localhost:3861', {
+      transports: ['websocket']
     });
+
+    socket.on('connect', () => {
+      console.log('worked...');
+      done();
+    });
+
+    socket.on('disconnect', () => {
+      console.log('disconnected...');
+    });
+  });
+
+  afterEach((done) => {
+    // Cleanup
+    if (socket.connected) {
+      console.log('disconnecting...');
+      socket.disconnect();
+    } else {
+      // There will not be a connection unless you have done() in beforeEach, socket.on('connect'..)
+      console.log('no connection to break...');
+    }
+    done();
+  });
 
   describe('testing test', () => {
     it('Should add a general data record', (done) => {
@@ -53,11 +53,11 @@ describe('Websocket tests', () => {
       socket.emit('get-initial-logs');
 
       socket.on('display-logs', (message) => {
-        expect(message.allLogs[message.allLogs.length-1]).toEqual(data);
+        expect(message.allLogs[message.allLogs.length - 1]).toEqual(data);
         done();
-      })
-    })
-    
+      });
+    });
+
     it('Should add a new record for console log', (done) => {
       const log = {
         type: 'log',
@@ -66,45 +66,43 @@ describe('Websocket tests', () => {
         timestamp: 'Sat, 10 Apr 2021 20:32:13 GMT',
       };
 
-      socket.emit('store-logs', log); 
+      socket.emit('store-logs', log);
 
       socket.emit('get-initial-logs');
 
       socket.on('display-logs', (message) => {
-        expect(message.allLogs[message.allLogs.length - 1].type).toEqual('log');
-        expect(message.allLogs[message.allLogs.length - 1].log).toEqual('setup');
+        expect(message.allLogs[message.allLogs.length - 1]).toEqual(log);
       });
-      
+
       done();
     });
 
     it('Should add a new record for a request', (done) => {
-        const req = [
-            {
-              class: 'request',
-              timestamp: 'Sat, 10 Apr 2021 20:32:13 GMT',
-              fromIP: '13.66.139.159',
-              method: 'GET',
-              originalUri: '/api/',
-              uri: '/api/',
-              requestData: { filter: 'test' },
-            },
-            {
-              class: 'response',
-              timestamp: 'Sat, 10 Apr 2021 20:32:30 GMT',
-              responseData: { 'data' : 'test' },
-              responseStatus: 200,
-              referer: '',
-            },
-          ];
+      const req = [
+        {
+          class: 'request',
+          timestamp: 'Sat, 10 Apr 2021 20:32:13 GMT',
+          fromIP: '13.66.139.159',
+          method: 'GET',
+          originalUri: '/api/',
+          uri: '/api/',
+          requestData: { filter: 'test' },
+        },
+        {
+          class: 'response',
+          timestamp: 'Sat, 10 Apr 2021 20:32:30 GMT',
+          responseData: { data: 'test' },
+          responseStatus: 200,
+          referer: '',
+        },
+      ];
 
-      socket.emit('store-logs', req); 
+      socket.emit('store-logs', req);
 
       socket.emit('get-initial-logs');
 
       socket.on('display-logs', (message) => {
-        expect(message.allLogs[message.allLogs.length - 1].type).toEqual('log');
-        expect(message.allLogs[message.allLogs.length - 1].log).toEqual('setup');
+        expect(message.allLogs[message.allLogs.length - 1]).toEqual(req);
       });
 
       done();
@@ -116,7 +114,7 @@ describe('Websocket tests', () => {
       socket.on('display-logs', (message) => {
         expect(message.allLogs.length).toBe(0);
         done();
-      })
+      });
     });
-  })
-})
+  });
+});
